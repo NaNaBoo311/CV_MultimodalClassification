412954226d99a676c0badcba3134986e5e65487c
NaNaBoo3101 <gtacbt0@gmail.com>
2026-04-12 23:35:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MultimodalClassification from './components/MultimodalClassification/MultimodalClassification';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <MultimodalClassification />
+    </div>
+  );
+}
+
+export default App;
